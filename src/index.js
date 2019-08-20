4ba0b851705f96c75eaa34e04ead60f7393e1e96
vedataydin <mvedataydin@gmail.com>
2019-08-20 23:51:08 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('inital setup test')
+import { compareAsc, format } from 'date-fns'
+import flatpickr from "flatpickr";
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//=> '2014-02-11'
+
+const dateDisplay = document.querySelector('.date-picker')
+const fp = flatpickr('.date-picker', {
+  altInput: true,
+  altFormat: "F j",
+  dateFormat: "Y-m-d",
+});
+
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10)
+]
+dates.sort(compareAsc)
+console.log(dates)
+//=> [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
