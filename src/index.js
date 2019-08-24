f996ef48e8f7c46c4847fc27bb21f90dad2efd02
vedataydin <mvedataydin@gmail.com>
2019-08-24 12:52:51 +0300

    complete functionality of todo app and made visual improvements

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
 import { compareAsc, format } from 'date-fns'
 import flatpickr from "flatpickr";
 import { createDom } from './modules/createDom.js'
+import { initial } from './modules/initial.js'
 import { todoFactory } from './modules/todoo.js'
 import { projects} from './modules/projects.js'
-import { addTodo,showForm} from './modules/handlers.js'
-import { renderProjects, renderTodos,renderAddTodo} from './modules/render.js'
+import { addTodo,todoFormListener, projectFormListener} from './modules/handlers.js'
+import { renderProjects,renderTButton, renderTodos,renderAddTodo} from './modules/render.js'
+
+projectFormListener();
+initial();
 
 
-showForm();
 
 
-
-// format(new Date(2014, 1, 11), 'yyyy-MM-dd')
-// //=> '2014-02-11'
-
-// const dateDisplay = document.querySelector('.date-picker')
-// const fp = flatpickr('.date-picker', {
-//   altInput: true,
-//   altFormat: "F j",
-//   dateFormat: "Y-m-d",
-// });
-
-
-// const dates = [
-//   new Date(1995, 6, 2),
-//   new Date(1987, 1, 11),
-//   new Date(1989, 6, 10)
-// ]
-// dates.sort(compareAsc)
-// console.log(dates)
-//=> [
-//   Wed Feb 11 1987 00:00:00,
-//   Mon Jul 10 1989 00:00:00,
-//   Sun Jul 02 1995 00:00:00
-// ]
